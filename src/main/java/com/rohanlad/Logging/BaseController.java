2ae6dccce48afbfd5575cd18936240943ab1766f
Rohan Lad <32235215+rohanlad23@users.noreply.github.com>
2021-08-05 21:28:03 +0530

    Standard way to log is added!

---
+++ src/main/java/com/rohanlad/Logging/BaseController.java
@@ -0,0 +1,16 @@
+package com.rohanlad.Logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+    Logger logger= (Logger) LoggerFactory.getLogger(BaseController.class);
+    @RequestMapping("/")
+    public String welcome(){
+        logger.error("error occurred!");
+        return "welcome!";
+    }
+}
