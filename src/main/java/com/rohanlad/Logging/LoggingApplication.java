8d0278870d9572706666ae9adc9c331ef69c528f
Rohan Lad <32235215+rohanlad23@users.noreply.github.com>
2021-08-05 21:10:32 +0530

    initial commit

---
+++ src/main/java/com/rohanlad/Logging/LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.rohanlad.Logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingApplication.class, args);
+	}
+
+}
